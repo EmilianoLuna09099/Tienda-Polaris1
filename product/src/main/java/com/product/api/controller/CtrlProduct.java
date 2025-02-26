193f220ad666d86ee9a50228c280016ebc0e348f
Emiliano Luna <emilianolunac09@ciencias.unam.mx>
2025-02-26 17:15:23 -0600

    Impliementacion de lab

--- product/src/main/java/com/product/api/controller/CtrlProduct.java
+++ product/src/main/java/com/product/api/controller/CtrlProduct.java
@@ -7,6 +7,7 @@ import com.product.api.service.SvcCategory;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class CtrlProduct {
 	SvcCategory svc;
 	
 	@GetMapping
-	public List<Category> getCategorias() {
+	public ResponseEntity< List<Category>> getCategorias() {
 		return svc.getCategories();
 	}
 
--- product/src/main/java/com/product/api/service/SvcCategory.java
+++ product/src/main/java/com/product/api/service/SvcCategory.java
@@ -1,9 +1,12 @@
 package com.product.api.service;
 
 import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
 import com.product.api.entity.*;
 
 public interface SvcCategory{
 	
-	public List<Category> getCategories();
+	public ResponseEntity< List<Category>> getCategories();
 }
